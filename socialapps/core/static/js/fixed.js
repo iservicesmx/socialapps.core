f5c9911348c0a86988272439ff5dbfb6627d6bb9
Elena Vazquez <ele.vqz@gmail.com>
2011-09-07 13:34:45 -0500

    css js ie7 ie8

--- socialapps/core/static/js/fixed.js
+++ socialapps/core/static/js/fixed.js
@@ -1,12 +1,7 @@
-/*
-
-jQuery(function($){
-	$('dt:nth-child(even)').addClass('evenDts');
-
-});
-*/
-
 	$(document).ready(function() {
+		$('#content').corner("round 0.333em");
+		$('.block-content H1').corner("cc:#000");		
+		$('UL#navigation LI A').corner("cc:#8bc8ec");
 		$('tr:nth-child(even)').addClass('evenDts');
 		$('tr.odd').addClass('oddDtr');	
 		$('tr:nth-child(odd)').addClass('oddNtr');		
