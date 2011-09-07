a739d2ceac7989c8cc535f3ac62c9248cb4521d2
Elena Vazquez <ele.vqz@gmail.com>
2011-09-07 14:04:06 -0500

    dash ie

--- socialapps/core/static/js/fixed.js
+++ socialapps/core/static/js/fixed.js
@@ -2,6 +2,8 @@
 		$('#content').corner("round 0.333em");
 		$('.block-content H1').corner("cc:#000");		
 		$('UL#navigation LI A').corner("cc:#8bc8ec");
+		$('#nav-menu, .title').corner("top");		
+		$('#nav-menu .current a, ul#nav-menu li a:hover').corner("round 3px");		
 		$('tr:nth-child(even)').addClass('evenDts');
 		$('tr.odd').addClass('oddDtr');	
 		$('tr:nth-child(odd)').addClass('oddNtr');		
