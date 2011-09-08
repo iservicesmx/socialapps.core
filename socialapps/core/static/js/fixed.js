265a91f1d710326274e5a1091d2d92cca1804289
Elena Vazquez <ele.vqz@gmail.com>
2011-09-08 03:06:36 -0500

    js ie

--- socialapps/core/static/js/fixed.js
+++ socialapps/core/static/js/fixed.js
@@ -3,10 +3,9 @@
 		$('.block-content H1').corner("cc:#000");		
 		$('UL#navigation LI A').corner("cc:#8bc8ec");
 		$('#nav-menu, .title').corner("top");
-/*		$('#nav-menu, .title').css('box-shadow','0 1px 3px #222;');*/
-		$('#nav-menu .current a, ul#nav-menu li a:hover').corner("round 3px");		
+		$('#nav-menu .current A').corner("round 3px");		
 		$('tr:nth-child(even)').addClass('evenDts');
 		$('tr.odd').addClass('oddDtr');	
 		$('tr:nth-child(odd)').addClass('oddNtr');		
-		$('.button').corner("round 0.5em");			
+		$('.button').corner("round 0.5em");		
 	});
