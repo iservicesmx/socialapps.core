d90dbbd5675d0f1443b8ebf995113af5d51a507c
Isaac I. Torres <istodi@gmail.com>
2012-02-14 12:36:16 -0600

    new browser for cleditor

--- socialapps/core/static/js/jquery.cleditor.extimage.js
+++ socialapps/core/static/js/jquery.cleditor.extimage.js
@@ -39,35 +39,38 @@
 		var editor = data.editor,
 			$text = $(data.popup).find(':text.url'),
 			url = $.trim($text.val()),
-            $existing = $(data.popup).find('iframe[name="existing"]').contents().find('.selected-size');
+            $existing = $(data.popup).find('iframe[name="existing"]').contents().find('.selected-size'),
 			$iframe = $(data.popup).find('iframe[name="__upload_iframe"]'),
 			$file = $(data.popup).find(':file');
 
 		// clear previously selected file and url
-		$file.val('');
-        $(data.popup).find('iframe[name="existing"]').contents().find('.selected-object').hide();
-		$existing.attr('value', '');
-		
-        var $tabs = $('.browser-tabs a:first').tab('show');
-        var selected = '#url';
-        
+        $file.val('');
+        $iframe.contents().find('body').html('');
+        $existing.attr('value', '');
+
+        $('.browser-tabs a:first').tab('show');
+		var selected_image = $('.browser-tabs li.active').attr('href');
         $('.browser-tabs a[data-toggle="tab"]').on('shown', function(e) {
-            selected = $(e.target).attr('href');
+            selected_image = $(e.target).attr('href');
         });
         
-        $(':button', data.popup).bind('click', function(e) {
-            $existing = $(data.popup).find('iframe[name="existing"]').contents().find('.selected-size').val();
-            if (selected == '#existing' && $existing != undefined) {
-                editor.execCommand(data.command, $existing, null, data.button);
-                closePopup(editor);
-            } else if(selected == '#upload' && $file.val()) { // proceed if any file was selected
+        $(data.popup).find('.save-image').unbind('click').bind('click', function(e) {
+            console.log(selected_image);
+            if (selected_image == '#existing') {
+                var existing = $(data.popup).find('iframe[name="existing"]').contents().find('.selected-size').val();
+                console.log(existing);
+                if (existing != 'undefined') {
+                    editor.execCommand(data.command, existing, null, data.button);
+                    closePopup(editor);                    
+                }
+            } else if(selected_image == '#upload' && $file.val() != '') { // proceed if any file was selected
                 $iframe.bind('load', function() {
-                    var file_url;
                     try {
-                        file_url = $iframe.get(0).contentWindow.document.body.innerHTML;
+                        var file_url = $iframe.contents().find("pre").html();
                     } catch(e) {};
                     if(file_url) {
-                        editor.execCommand(data.command, file_url, null, data.button);
+                        var obj = $.parseJSON(file_url).image;
+                        editor.execCommand(data.command, obj, null, data.button);
                     } else {
                         alert('An error occured during upload!');
                     }
@@ -75,7 +78,7 @@
                     closePopup(editor);
                 });
                 $(data.popup).find('form').attr('action', $.cleditor.buttons.image.uploadUrl).submit();
-            } else if (selected == '#url' && $text.val() != '') {
+            } else if (selected_image == '#url' && $text.val() != '') {
                 editor.execCommand(data.command, $text.val(), null, data.button);
                 closePopup(editor);
             }
@@ -87,30 +90,18 @@
 			$text = $(data.popup).find(':text.url'),
 			url = $.trim($text.val()),
             $existing = $(data.popup).find('iframe[name="existing"]').contents().find('.url');
-
-        if (editor.selectedText() === "") {
-            editor.showMessage('A selection is required when inserting a link', data.button);
-            return false;
-        }
             
-		// clear previously selected file and url
-		$text.val('').focus();
-        $(data.popup).find('iframe[name="existing"]').contents().find('.selected-url').hide();
-		$existing.attr('value', '');
-		
-        var $tabs = $('.browser-tabs a:first').tab('show');
-        var selected = '#link-url';
-        
-        $('.browser-tabs a[data-toggle="tab"]').on('shown', function(e) {
-            selected = $(e.target).attr('href');
-        });
-
+        $('.browser-url-tabs a:first').tab('show');
+		var selected_url = $('.browser-url-tabs li.active').attr('href');
+        $('.browser-url-tabs a[data-toggle="tab"]').on('shown', function (e) {
+            selected_url = $(e.target).attr('href');
+        })        
         $(data.popup).find('.save-link').unbind('click').bind('click', function(e) {
             $existing = $(data.popup).find('iframe[name="existing"]').contents().find('.url').val();
-            if (selected == '#existing-url' && $existing != undefined) {
+            if (selected_url == '#existing-url' && $existing != undefined) {
                 editor.execCommand(data.command, $existing, $existing, data.button);
                 closePopup(editor);
-            } else if (selected == '#link-url' && $text.val() != '') {
+            } else if (selected_url == '#link-url' && $text.val() != '') {
                 editor.execCommand(data.command, $text.val(), $text.val(), data.button);
                 closePopup(editor);
             }            
