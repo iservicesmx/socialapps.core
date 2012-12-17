4ca579a6f1c7783dfd587cbad021e0d27beaef9f
Isaac Torres <istodi@gmail.com>
2012-12-17 12:59:06 -0600

    permission mixin

--- socialapps/core/mixins.py
+++ socialapps/core/mixins.py
@@ -6,13 +6,17 @@ from django.utils.translation import ugettext_lazy as _
 class PermissionMixin(object):
     permission = "edit"
 
-
     def dispatch(self, request, *args, **kwargs):
         self.request = request
         self.args = args
         self.kwargs = kwargs
-        if hasattr(self, 'get_object'):
-            perm = has_permission(self.get_object(), request.user, self.permission)
+        obj = None
+        if hasattr(self, 'perm_object'):
+            obj = self.perm_object()
+        elif hasattr(self, 'get_object'):
+            obj = self.get_object()
+        if obj:
+            perm = has_permission(obj, request.user, self.permission)
             if perm:
                 return super(PermissionMixin, self).dispatch(request, *args, **kwargs);
             raise Http404
