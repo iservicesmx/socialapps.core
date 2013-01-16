4eec2abe5e0b1cdac19496de8e0c38252fac70a6
Erik Rivera <erik.river@gmail.com>
2013-01-15 19:20:12 -0600

    site email

--- socialapps/core/notifications.py
+++ socialapps/core/notifications.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.template import Context
+from django.utils.translation import ugettext
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
 from notification.backends import email
 
@@ -23,3 +26,30 @@ class EmailCustomBackend(email.EmailBackend):
             "current_site": current_site,
             "base_url": base_url
         })
+        
+    def deliver(self, recipient, sender, notice_type, extra_context):
+        # TODO: require this to be passed in extra_context
+
+        context = self.default_context()
+        context.update({
+            "recipient": recipient,
+            "sender": sender,
+            "notice": ugettext(notice_type.display),
+        })  
+        context.update(extra_context)
+
+        messages = self.get_formatted_messages((
+            "short.txt",
+            "full.txt"
+        ), notice_type.label, context)
+
+        subject = "".join(render_to_string("notification/email_subject.txt", {
+            "message": messages["short.txt"],
+        }, context).splitlines())
+
+        body = render_to_string("notification/email_body.txt", {
+            "message": messages["full.txt"],
+        }, context)
+        # TODO: remove school dependency
+        send_mail(subject, body, "%s <%s>" %(self.site.school.title, self.site.school.email), [recipient.email])
+        
